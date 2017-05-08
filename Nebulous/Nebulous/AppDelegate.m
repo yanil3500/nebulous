9764836a618750fed0ca2877349389ccb768720a
yanil3500 <yanil3500@gmail.com>
2017-05-08 11:17:22 -0700

    adds api key for darksky

--- Nebulous/Nebulous/AppDelegate.m
+++ Nebulous/Nebulous/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Forecastr+CLLocation.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [Forecastr sharedManager];
     // Override point for customization after application launch.
     return YES;
 }
